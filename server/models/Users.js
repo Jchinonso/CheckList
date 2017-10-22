3ee153dabad3b6ce096e0a741226584d22f52fb7
andela-jchinonso <chinonso.johnson@andela.com>
2017-10-22 12:36:17 +0100

    [feature]: add user schema

---
+++ server/models/Users.js
@@ -0,0 +1,122 @@
+
+import mongoose from 'mongoose';
+import titlize from 'mongoose-title-case';
+import validate from 'mongoose-validator';
+import bcrypt from 'bcrypt-nodejs';
+
+const nameValidator = [
+  validate({
+      validator: 'matches',
+      arguments: /^(([a-zA-Z]{3,20})+[ ]+([a-zA-Z]{3,20})+)+$/,
+      message: 'Name must be at least 3 characters, max 30, no special characters or numbers, must have space in between name.'
+  }),
+  validate({
+      validator: 'isLength',
+      arguments: [3, 20],
+      message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+
+
+const emailValidator = [
+  validate({
+      validator: 'isEmail',
+      message: 'Email is Invalid'
+  })
+];
+
+
+const usernameValidator = [
+  validate({
+      validator: 'isLength',
+      arguments: [3, 25],
+      message: 'Username should be between {ARGS[0]} and {ARGS[1]} characters'
+  }),
+  validate({
+      validator: 'isAlphanumeric',
+      message: 'Username must contain letters and numbers only'
+  })
+];
+
+// Password Validator
+const passwordValidator = [
+  validate({
+      validator: 'isLength',
+      arguments: [8, 35],
+      message: 'Password should be between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    validate: nameValidator
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: usernameValidator
+  },
+  password: {
+     type: String,
+     required: true,
+     trim: true,
+     validate: passwordValidator,
+     select: false
+    },
+  email: {
+     type: String,
+     required: true,
+     unique: true,
+     lowercase: true,
+     validate: emailValidator
+    },
+  image: {
+     type: Buffer
+    },
+  created_at: {
+    type: Date,
+    default: Date.now()
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+  });
+});
+
+
+UserSchema.plugin(titlize, {
+  paths: ['name']
+});
+
+
+UserSchema.methods.comparePassword = function(password) {
+  const user = this;
+  return bcrypt.compareSync(password, user.password);
+};
+
+
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
