4ae6298f16a669fb9c3842a574c71257cbf17c56
andela-jchinonso <chinonso.johnson@andela.com>
2017-10-24 16:17:34 +0100

    [feature]: add retrieve all user route

--- server/routes/TodosRoutes.js
+++ server/routes/TodosRoutes.js
@@ -1,15 +1,18 @@
-// import TodosController from '../controllers/GroupController';
+ import TodosController from '../controllers/TodoController';
+ import Auth from '../middleware/Auth';
 
-// const TodosRoutes = (router) => {
-//     router.route('/todos')
-//         .post(Auth.verifyToken, TodosController.createTodos)
-//         .get(Auth.verifyToken, TodosController.retrieveAllTodos);
-//     router.route('/todos/:id/task')
-//         .post(Auth.verifyToken, TodosController.addTask)
-//         .get(Auth.verifyToken, TodosController.retrieveTasks);
-//     router.route('/todos/:id/users')
-//         .post(Auth.verifyToken, TodosController.addUsersToTodos)
+const TodosRoutes = (router) => {
+  router.route('/todo')
+    .post(Auth.verifyToken, TodosController.createTodos)
+    .get(Auth.verifyToken, TodosController.retrieveAllTodos)
+  router.route('/todos/:todoId/task')
+    .post(Auth.verifyToken, TodosController.addTask)
+  router.route('/todos/task/:taskId')
+    .put(Auth.verifyToken, TodosController.updateTask)
+    .delete(Auth.verifyToken, TodosController.deleteTask)
+  router.route('/todos/:todoId/collaborator')
+    .post(Auth.verifyToken, TodosController.addColaborator)
 //         .get(Auth.verifyToken, TodosController.retrieveTodoUsers);
-//   };
+};
 
-// export default TodosRoutes;
+export default TodosRoutes;
