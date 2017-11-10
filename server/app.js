4844b3509c5392b3b40bdf51845ea4c8a43dfb92
andela-jchinonso <chinonso.johnson@andela.com>
2017-11-10 08:18:54 +0100

    [chore]: configure app for deployment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import webpack from 'webpack';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 import mongoose from 'mongoose';
+import compression from 'compression';
 import config from './../webpack.config.dev';
 import Routes from './routes/index';
 
@@ -28,12 +29,18 @@ const app = express();
 const router = express.Router();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+if (NODE_ENV === 'development') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
-app.use(require('webpack-hot-middleware')(compiler));
+if (NODE_ENV !== 'developmemt') {
+  app.use(compression());
+  app.use(express.static('dist'));
+}
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -44,9 +51,16 @@ Routes(router);
 // prefix /api for all routes
 app.use('/api/v1', router);
 
+if (NODE_ENV === 'developmemt') {
+  app.get('*', (req, res) => res.status(200)
+    .sendFile(path.join(__dirname, './../client', 'index.html')));
+}
 
-app.get('*', (req, res) => res.status(200)
-  .sendFile(path.join(__dirname, './../client', 'index.html')));
+if (NODE_ENV !== 'development') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
 
 
 module.exports = app;
